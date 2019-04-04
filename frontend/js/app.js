d2099df54d9b6b6e3b22b463ecb8839a2c47008a
Smirennikov <evgenii.smirennikov@gmail.com>
2019-04-04 16:17:39 +0300

    add animation to calendar

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,4 @@
 import svgSprite from './inc/svg-sprite';
-
+import controlCalendar from './controlCalendar'
 svgSprite(window, document)
+controlCalendar()
---
+++ frontend/js/controlCalendar.js
@@ -0,0 +1,25 @@
+function controlCalendar(){
+  const root = document;
+  const body = root.body;
+  const calendars = root.getElementsByClassName('article-calendar');
+
+  for( let i = 0, max = calendars.length; i < max; i++){
+    calendars[i].addEventListener('click', function(){
+
+      if(body.getAttribute('calendar')){
+        body.setAttribute('calendar', '')
+      }else{
+        body.setAttribute('calendar', 'open')
+      }
+
+      for( let i = 0, max = calendars.length; i < max; i++){
+        calendars[i].classList.toggle('active')
+      }
+
+    })
+  }
+
+
+}
+
+export default controlCalendar
